a9bde36f49ef2e991aafbe40be1630728d93a0bb
BaselHussain <154091057+BaselHussain@users.noreply.github.com>
2024-12-11 16:10:47 +0500

    quiz-app

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import QuizComponent from "@/components/quiz-app";
+import Spinner from "@/components/ui/Spinner";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <QuizComponent/>
+   </div>
+  );
+}
---
+++ components/ui/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <div>
+      <div className=' border-2 border-gray-600 w-6 h-6 rounded-full animate-spin border-r-transparent'></div>
+    </div>
+  )
+}
